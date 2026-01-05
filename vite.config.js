768234609a45634c61a24f5804f0b1f333376ebd
Shad Anwar <shad93709@gmail.com>
2026-01-05 20:16:43 +0530

    Remove node_modules and dist from tracking

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BOOKS-STORE/",   // 👈 repo name EXACT
+  base: "/BOOKS-STORE/",   // 👈 MUST match repo name
 })
